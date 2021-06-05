7281f345a894ae87764e0629a563ffb169ea5972
Ayangchao <403181507@qq.com>
2021-06-05 16:45:54 +0800

    修改 dataId

--- src/main/java/com/regist/nacos/NacosTestApplication.java
+++ src/main/java/com/regist/nacos/NacosTestApplication.java
@@ -7,7 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
-@NacosPropertySource(dataId = "application.yml", autoRefreshed = true, groupId = "NACOS-TEST-GROUP")
+@NacosPropertySource(dataId = "nacos-test-dev.yml", autoRefreshed = true, groupId = "NACOS-TEST-GROUP")
 public class NacosTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(NacosTestApplication.class, args);
