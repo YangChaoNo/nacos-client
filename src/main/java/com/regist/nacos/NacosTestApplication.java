fb5604dae8a0775af21ffb0189e47d51c4d90723
yangchao <43333680+YangChaoNo@users.noreply.github.com>
2021-06-05 16:33:15 +0800

    Add files via upload

---
+++ src/main/java/com/regist/nacos/NacosTestApplication.java
@@ -0,0 +1,15 @@
+package com.regist.nacos;
+
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+@NacosPropertySource(dataId = "application.yml", autoRefreshed = true, groupId = "NACOS-TEST-GROUP")
+public class NacosTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosTestApplication.class, args);
+    }
+}
